d7bbbbcab7ae936d93f5c547b54164cb8d02a13e
Artem Sydorov <sydorovay@gmail.com>
2023-03-25 15:01:02 +0100

    finish task 7/2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,30 @@
+import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+// console.log(galleryItems);
 
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+//callback. створити розмітку і повернути динамічний рядок
+function createGallery(items) {
+  return items.reduce((acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`
+    );
+  }, "");
+}
+
+// // перебрати galleryItems з допомогою callback ф-ції
+const galleryMarkup = createGallery(galleryItems);
+
+// //додати розмітку в html
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+//Підпис зображень с затримкою 
+const lightboxGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
